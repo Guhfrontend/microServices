24923b065bc88f723f68a67a9f13e1a454d29501
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-08-09 17:40:57 -0300

    restTamplate entre Worker e payroll

--- hr-payroll/src/main/java/com/devsuperior/hr_payroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hr_payroll/service/PaymentService.java
@@ -1,14 +1,33 @@
 package com.devsuperior.hr_payroll.service;
 
 import com.devsuperior.hr_payroll.model.Payment;
+import com.devsuperior.hr_payroll.model.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariabel = new HashMap<>();
+        uriVariabel.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariabel);
+
+        return new Payment(worker.getName(),worker.getDailyIncome(),days);
     }
+
+
 }
