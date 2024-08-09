4dd816cb7196b0daf45f9f0ca14a84e544b84d82
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-08-09 18:13:58 -0300

    rabbion balancing- balanciar entre portas

--- hr-payroll/src/main/java/com/devsuperior/hr_payroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hr_payroll/service/PaymentService.java
@@ -4,14 +4,7 @@ import com.devsuperior.hr_payroll.feignclients.WorkerFeignClient;
 import com.devsuperior.hr_payroll.model.Payment;
 import com.devsuperior.hr_payroll.model.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
--- hr-worker/src/main/java/com/devsuperior/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hr_worker/controller/WorkerController.java
@@ -2,16 +2,24 @@ package com.devsuperior.hr_worker.controller;
 
 import com.devsuperior.hr_worker.model.Worker;
 import com.devsuperior.hr_worker.repository.WorkerRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
+
+    private static Logger logger = Logger.getLogger(WorkerController.class.getName());
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -22,7 +30,10 @@ public class WorkerController {
     }
 
     @GetMapping(value = "{id}")
+
     private ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("Port= " + env.getProperty("local.server.port"));
+
         return workerRepository.findById(id).map(res -> ResponseEntity.ok(res)).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
