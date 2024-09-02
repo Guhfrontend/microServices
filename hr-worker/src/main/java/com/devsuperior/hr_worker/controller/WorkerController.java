f918a34606a9b482af8476177f058c9d4a3774fc
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-09-02 20:19:10 -0300

    zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hr_worker/controller/WorkerController.java
@@ -31,6 +31,12 @@ public class WorkerController {
 
     @GetMapping(value = "{id}")
     private ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        try{
+            Thread.sleep(3000L);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
         logger.info("Port= " + env.getProperty("local.server.port"));
 
         return workerRepository.findById(id).map(res -> ResponseEntity.ok(res)).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
