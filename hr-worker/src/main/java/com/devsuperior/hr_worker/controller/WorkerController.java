b50e797262a61f2b413696f420f900de530c8ef6
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-09-05 15:15:33 -0300

    private git repository config

--- hr-worker/src/main/java/com/devsuperior/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hr_worker/controller/WorkerController.java
@@ -2,7 +2,6 @@ package com.devsuperior.hr_worker.controller;
 
 import com.devsuperior.hr_worker.model.Worker;
 import com.devsuperior.hr_worker.repository.WorkerRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -21,9 +20,8 @@ public class WorkerController {
 
     private static Logger logger = Logger.getLogger(WorkerController.class.getName());
 
-  // @Value("${test.config}")
-  //private String testConfig;
-
+    //@Value("${test.config}")
+    //private String testConfig;
 
     @Autowired
     private Environment env;
@@ -31,29 +29,28 @@ public class WorkerController {
     @Autowired
     private WorkerRepository workerRepository;
 
-
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-
+    //    logger.info("CONFIG = " + testConfig);
         logger.info("CONFIG = " + env.getProperty("local.server.port"));
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    private ResponseEntity<List<Worker>> findyall(){
+    public ResponseEntity<List<Worker>> findAll() {
         return ResponseEntity.ok(workerRepository.findAll());
     }
 
     @GetMapping(value = "{id}")
-    private ResponseEntity<Worker> findById(@PathVariable Long id){
-
-        try{
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        try {
             Thread.sleep(3000L);
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         logger.info("Port= " + env.getProperty("local.server.port"));
-
-        return workerRepository.findById(id).map(res -> ResponseEntity.ok(res)).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        return workerRepository.findById(id)
+                .map(res -> ResponseEntity.ok(res))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
